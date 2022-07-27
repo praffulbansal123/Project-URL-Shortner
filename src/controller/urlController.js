eba74c3ffa1b0b5b65b6ff535a0cd79e2dbfe347
praffulbansal123 <prafful.bansal@gmail.com>
2022-07-27 14:12:39 +0530

    Commenting done in controller

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -15,6 +15,7 @@ redisClient.auth("yYakwkjGgnOPwetUbnN8GgtEkK5i3bth", function(err) {
     if (err) throw err;
 });
 
+//Redis Connection
 redisClient.on("connect", async function() {
     console.log("connected to Redis..");
 });
@@ -22,30 +23,34 @@ redisClient.on("connect", async function() {
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
+
 //---------------------------Creating URL shortner--------------------------------------------------------//
-
-
-
 const urlShortner = async function (req, res, next) {
     try{
         const requestBody = req.body
         const requestQuery = req.query
 
+        //Query Params should be empty
         if(isValid(requestQuery)) throw createError.BadRequest('Invalid input as query params should be empty')
 
+        //Request Body should not be empty
         if(!isValid(requestBody)) throw createError.NotAcceptable('Input data should be of length 1')
 
+        //Base Url is taken from the readme
         const longUrl = requestBody.longUrl
         const baseUrl = "http://localhost:3000"
         const validLongUrl = await urlschema.extract("longUrl").validateAsync(longUrl)
 
         const urlFromDB = await UrlModel.findOne({longUrl: validLongUrl})
 
+        //Checking for the url in the DB
         if(urlFromDB) throw createError.Conflict('This Url has already been shorten and stored in the DB')
         
+        //Generating random code by using nanoid package
         const urlCode = nanoid()
         const shortUrl = baseUrl + '/' + urlCode
 
+        //Creating the url data
         const urlData = {
             urlCode: urlCode,
             longUrl: validLongUrl.trim(),
@@ -68,13 +73,23 @@ const getUrl = async function (req, res,next) {
         const requestQuery = req.query
         const urlCode = req.params.urlCode
 
+        //Query Params should be empty
         if(isValid(requestBody)) throw createError.BadRequest('No input data is required in the body')
 
+        //Request Body should not be empty
         if(isValid(requestQuery)) throw createError.BadRequest('Invalid Request as no data is required in query params')
 
+        // First checkING inside cache memory
+        const urlDataFromCache = await GET_ASYNC(urlCode);
+
+        if (urlDataFromCache) return res.status(302).redirect(urlDataFromCache)
+
+        // If cache miss, lets check in our DB, if available then populate the cache
         const urlFromUrlCode = await UrlModel.findOne({urlCode}).select({shortUrl: 1,longUrl: 1,_id: 0,})
 
         if(!urlFromUrlCode) throw createError.NotFound('No such url code exits in the DB')
+
+        const addingUrlDataInCache = SET_ASYNC(urlCode,urlFromUrlCode.longUrl);
 
         return res.status(200).redirect(urlFromUrlCode.longUrl)
     }
